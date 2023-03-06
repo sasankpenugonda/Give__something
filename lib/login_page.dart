7f88874bf3eaec3c110f840cd13a7cb83db8c077
Revanthk08 <revanth21103@gmail.com>
2023-03-06 21:13:04 +0530

    folder upload

---
+++ lib/login_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      // ignore: unnecessary_const
+      body: Column(children: const [
+        //Gap above
+        SizedBox(
+          height: 130,
+        ),
+        //welcomeback!
+        Text("Welcome Back!",
+        style: TextStyle(
+          fontFamily: "Poppins",
+          fontWeight: FontWeight.bold,
+          fontSize: 24
+        ),
+        ),
+        //text
+        Text("Nice to meet you! Please login to access \n back and start donating.",
+        style: TextStyle(
+          fontFamily: "Poppins",
+          fontWeight: FontWeight.bold,
+          fontSize: 18
+        ),
+        //email
+        //password
+        //login button
+      ]),
+    );
+  }
+}
